7a9caa74d2948749baebf274222ae702a4139f24
opheliah95 <opheliah95@gmail.com>
2022-10-11 17:59:35 +0100

    updated terraform provider

--- variables.tf
+++ variables.tf
@@ -1,4 +1,11 @@
-variable "token" {
-  type    = string
+variable "github_token" {
+  type        = string
   description = "github token"
+  default     = ""
+}
+
+variable "github_owner" {
+  type        = string
+  description = "owner of github repo"
+  default     = "opheliah95"
 }
